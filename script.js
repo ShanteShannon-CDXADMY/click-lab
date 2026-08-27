2c34cd19018fb896c1f464b5521eb3d28d9dd20b
Shante Shannon <shantesheree90@icloud.com>
2026-08-27 13:50:34 -0400

    add score board and audio to answers, closes #2, closes #3

--- script.js
+++ script.js
@@ -4,18 +4,45 @@ let tagline= document.getElementById("tagline-1");
 let button1 = document.getElementById("button-1");
 let button2 = document.getElementById("button-2");
 let button3 = document.getElementById("button-3");
+let score = 0;
+let scoreboard = document.getElementById("scoreboard");
 let correctSound = new Audio("correct.mp3");
-let wrongSound = new Audio ("wrong.mp3");
+let wrongSound = new Audio("wrong.mp3");
+
+
+function updateScoreboard() {
+  scoreboard.textContent = "Score: " + score;
+}
+
+function stopAllAudio() {
+  correctSound.pause();
+  correctSound.currentTime = 0;
+
+  wrongSound.pause();
+  wrongSound.currentTime = 0;
+}
 
 button1.addEventListener("click", function(){
-	tagline.textContent = "Incorrect!";
-	wrongSound.play();
+  stopAllAudio(); 
+  tagline.textContent = "Incorrect!";
+  tagline.style.color = "red";
+  wrongSound.play(); 
+ score--;  
 });
+
 button2.addEventListener("click", function(){
-	tagline.textContent = "Correct!";
-	correctSound.play();
+  stopAllAudio();
+  tagline.textContent = "Correct!";
+  tagline.style.color = "green";
+  correctSound.play(); 
+	score++;            
+  updateScoreboard(); 
 });
+
 button3.addEventListener("click", function(){
-	tagline.textContent = "Incorrect!";
-	wrongSound.play();
+  stopAllAudio();                
+  tagline.textContent = "Incorrect!";
+  tagline.style.color = "red";
+  wrongSound.play();    
+score--;  
 });
