2fc882e3a3e587f949af32a460784b08690efca3
Shante Shannon <shantesheree90@icloud.com>
2026-08-31 21:58:30 -0400

    the quiz has parts

--- script.js
+++ script.js
@@ -1,48 +1,97 @@
-let headline = document.getElementById("headline-1");
-console.log(headline);
-let tagline= document.getElementById("tagline-1");
-let button1 = document.getElementById("button-1");
-let button2 = document.getElementById("button-2");
-let button3 = document.getElementById("button-3");
 let score = 0;
 let scoreboard = document.getElementById("scoreboard");
-let correctSound = new Audio("correct.mp3");
-let wrongSound = new Audio("wrong.mp3");
 
+// Shared sounds
+let correctSound = document.getElementById("correctSound");
+let wrongSound = document.getElementById("wrongSound");
 
-function updateScoreboard() {
-  scoreboard.textContent = "Score: " + score;
+// Image groups
+let defaultImages = document.getElementsByClassName("defaultImage");
+let correctImages = document.getElementsByClassName("correctImage");
+let wrongImages = document.getElementsByClassName("wrongImage");
+
+// Hide all default images
+function hideDefaultImages() {
+  for (let img of defaultImages) {
+    img.style.display = "none";
+  }
 }
 
-function stopAllAudio() {
+// Hide all correct images
+function hideCorrectImages() {
+  for (let img of correctImages) {
+    img.style.display = "none";
+  }
+}
+
+// Hide all wrong images
+function hideWrongImages() {
+  for (let img of wrongImages) {
+    img.style.display = "none";
+  }
+}
+
+// Stop sounds
+function stopAudio() {
   correctSound.pause();
   correctSound.currentTime = 0;
-
   wrongSound.pause();
   wrongSound.currentTime = 0;
 }
 
-button1.addEventListener("click", function(){
-  stopAllAudio(); 
-  tagline.textContent = "Incorrect!";
-  tagline.style.color = "red";
-  wrongSound.play(); 
- score--;  
-});
+// Wrong answer
+function wrongAnswer(taglineElement, wrongImageId) {
+  hideDefaultImages();
+  hideCorrectImages();
+  hideWrongImages();
 
-button2.addEventListener("click", function(){
-  stopAllAudio();
-  tagline.textContent = "Correct!";
-  tagline.style.color = "green";
-  correctSound.play(); 
-	score++;            
-  updateScoreboard(); 
-});
+  document.getElementById(wrongImageId).style.display = "block";
 
-button3.addEventListener("click", function(){
-  stopAllAudio();                
-  tagline.textContent = "Incorrect!";
-  tagline.style.color = "red";
-  wrongSound.play();    
-score--;  
-});
+  taglineElement.textContent = "Incorrect!";
+  taglineElement.style.color = "red";
+
+  stopAudio();
+  wrongSound.play();
+
+  score--;
+  scoreboard.textContent = "Score: " + score;
+}
+
+// Correct answer
+function correctAnswer(taglineElement, correctImageId) {
+  hideDefaultImages();
+  hideCorrectImages();
+  hideWrongImages();
+
+  document.getElementById(correctImageId).style.display = "block";
+
+  taglineElement.textContent = "Correct!";
+  taglineElement.style.color = "green";
+
+  stopAudio();
+  correctSound.play();
+
+  score++;
+  scoreboard.textContent = "Score: " + score;
+}
+
+/* ---------------- QUESTION 1 ---------------- */
+let tagline1 = document.getElementById("tagline1");
+
+q1wrong1.addEventListener("click", () => wrongAnswer(tagline1, "wrongImage1"));
+q1wrong2.addEventListener("click", () => wrongAnswer(tagline1, "wrongImage1"));
+q1correct.addEventListener("click", () => correctAnswer(tagline1, "correctImage1"));
+
+/* ---------------- QUESTION 2 ---------------- */
+let tagline2 = document.getElementById("tagline2");
+
+q2wrong1.addEventListener("click", () => wrongAnswer(tagline2, "wrongImage2"));
+q2wrong2.addEventListener("click", () => wrongAnswer(tagline2, "wrongImage2"));
+q2correct.addEventListener("click", () => correctAnswer(tagline2, "correctImage2"));
+
+/* ---------------- QUESTION 3 ---------------- */
+let tagline3 = document.getElementById("tagline3");
+
+q3wrong1.addEventListener("click", () => wrongAnswer(tagline3, "wrongImage3"));
+q3wrong2.addEventListener("click", () => wrongAnswer(tagline3, "wrongImage3"));
+q3correct.addEventListener("click", () => correctAnswer(tagline3, "correctImage3"));
