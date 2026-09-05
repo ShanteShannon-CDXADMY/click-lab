2ee5efd117bacf8009f783ec7b50e1e9a3a958bd
Shante Shannon <shantesheree90@icloud.com>
2026-09-05 04:35:44 -0400

    the project is complete

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let score = 0;
 let scoreboard = document.getElementById("scoreboard");
 
 function updateScoreboard() {
-  scoreboard.textContent = "Score: " + score;
+	scoreboard.textContent = "Score: " + score;
 }
 
 // ======================================================
@@ -26,46 +26,46 @@ q1wrongText.style.display = "none";
 q1correctText.style.display = "none";
 
 q1correct.addEventListener("click", () =>{
-  defaultImage1.src="peru.jpg";
-  defaultImage1.alt = "correct image";
-  q1correctText.style.display = "block";
-  q1wrongText.style.display = "none";
-  q1correctText.style.color="green";
-  correctSound.pause();
-  correctSound.currentTime = 0;
-  correctSound.play();
-  score++;
-  updateScoreboard();
+	defaultImage1.src="peru.jpg";
+	defaultImage1.alt = "correct image";
+	q1correctText.style.display = "block";
+	q1wrongText.style.display = "none";
+	q1correctText.style.color="green";
+	correctSound.pause();
+	correctSound.currentTime = 0;
+	correctSound.play();
+	score++;
+	updateScoreboard();
 });
 
 q1wrong1.addEventListener("click", () => {
-  defaultImage1.src ="wrong.png";
-  defaultImage1.alt = "wrong image";
-  q1wrongText.style.display = "block";
-  q1correctText.style.display = "none";
-  q1wrongText.style.color="red";
-  wrongSound.pause();
-  wrongSound.currentTime = 0;
-  wrongSound.play();
-  score0
-  updateScoreboard();
+	defaultImage1.src ="wrong.png";
+	defaultImage1.alt = "wrong image";
+	q1wrongText.style.display = "block";
+	q1correctText.style.display = "none";
+	q1wrongText.style.color="red";
+	wrongSound.pause();
+	wrongSound.currentTime = 0;
+	wrongSound.play();
+	score0
+	updateScoreboard();
 });
 
 q1wrong2.addEventListener("click", () => {
-  defaultImage1.src ="wrong.png";
-  defaultImage1.alt = "wrong image";
-  q1wrongText.style.display = "block";
-  q1correctText.style.display = "none";
-  q1wrongText.style.color="red";
-  wrongSound.pause();
-  wrongSound.currentTime = 0;
-  wrongSound.play();
-  score--;
-  updateScoreboard();
+	defaultImage1.src ="wrong.png";
+	defaultImage1.alt = "wrong image";
+	q1wrongText.style.display = "block";
+	q1correctText.style.display = "none";
+	q1wrongText.style.color="red";
+	wrongSound.pause();
+	wrongSound.currentTime = 0;
+	wrongSound.play();
+	score--;
+	updateScoreboard();
 });
 
 // ======================================================
-// ⭐ QUESTION 2 (same format as Q1)
+//  QUESTION 2
 // ======================================================
 let defaultImage2 = document.getElementById("defaultImage2");
 let q2wrongText = document.getElementById("q2wrongText");
@@ -79,46 +79,46 @@ q2wrongText.style.display = "none";
 q2correctText.style.display = "none";
 
 q2correct.addEventListener("click", () =>{
-  defaultImage2.src="alaska.png";
-  defaultImage2.alt = "correct image";
-  q2correctText.style.display = "block";
-  q2wrongText.style.display = "none";
-  q2correctText.style.color="green";
-  correctSound.pause();
-  correctSound.currentTime = 0;
-  correctSound.play();
-  score++;
-  updateScoreboard();
+	defaultImage2.src="alaska.png";
+	defaultImage2.alt = "correct image";
+	q2correctText.style.display = "block";
+	q2wrongText.style.display = "none";
+	q2correctText.style.color="green";
+	correctSound.pause();
+	correctSound.currentTime = 0;
+	correctSound.play();
+	score++;
+	updateScoreboard();
 });
 
 q2wrong1.addEventListener("click", () => {
-  defaultImage2.src ="wrong.png";
-  defaultImage2.alt = "wrong image";
-  q2wrongText.style.display = "block";
-  q2correctText.style.display = "none";
-  q2wrongText.style.color="red";
-  wrongSound.pause();
-  wrongSound.currentTime = 0;
-  wrongSound.play();
-  score--;
-  updateScoreboard();
+	defaultImage2.src ="wrong.png";
+	defaultImage2.alt = "wrong image";
+	q2wrongText.style.display = "block";
+	q2correctText.style.display = "none";
+	q2wrongText.style.color="red";
+	wrongSound.pause();
+	wrongSound.currentTime = 0;
+	wrongSound.play();
+	score--;
+	updateScoreboard();
 });
 
 q2wrong2.addEventListener("click", () => {
-  defaultImage2.src ="wrong.png";
-  defaultImage2.alt = "wrong image";
-  q2wrongText.style.display = "block";
-  q2correctText.style.display = "none";
-  q2wrongText.style.color="red";
-  wrongSound.pause();
-  wrongSound.currentTime = 0;
-  wrongSound.play();
-  score--;
-  updateScoreboard();
+	defaultImage2.src ="wrong.png";
+	defaultImage2.alt = "wrong image";
+	q2wrongText.style.display = "block";
+	q2correctText.style.display = "none";
+	q2wrongText.style.color="red";
+	wrongSound.pause();
+	wrongSound.currentTime = 0;
+	wrongSound.play();
+	score--;
+	updateScoreboard();
 });
 
 // ======================================================
-// ⭐ QUESTION 3
+//  QUESTION 3
 // ======================================================
 let defaultImage3 = document.getElementById("defaultImage3");
 let q3wrongText = document.getElementById("q3wrongText");
@@ -132,40 +132,40 @@ q3wrongText.style.display = "none";
 q3correctText.style.display = "none";
 
 q3correct.addEventListener("click", () =>{
-  defaultImage3.src="saturn.jpg";
-  defaultImage3.alt = "correct image";
-  q3correctText.style.display = "block";
-  q3wrongText.style.display = "none";
-  q3correctText.style.color="green";
-  correctSound.pause();
-  correctSound.currentTime = 0;
-  correctSound.play();
-  score++;
-  updateScoreboard();
+	defaultImage3.src="saturn.jpg";
+	defaultImage3.alt = "correct image";
+	q3correctText.style.display = "block";
+	q3wrongText.style.display = "none";
+	q3correctText.style.color="green";
+	correctSound.pause();
+	correctSound.currentTime = 0;
+	correctSound.play();
+	score++;
+	updateScoreboard();
 });
 
 q3wrong1.addEventListener("click", () => {
-  defaultImage3.src ="wrong.png";
-  defaultImage3.alt = "wrong image";
-  q3wrongText.style.display = "block";
-  q3correctText.style.display = "none";
-  q3wrongText.style.color="red";
-  wrongSound.pause();
-  wrongSound.currentTime = 0;
-  wrongSound.play();
-  score--;
-  updateScoreboard();
+	defaultImage3.src ="wrong.png";
+	defaultImage3.alt = "wrong image";
+	q3wrongText.style.display = "block";
+	q3correctText.style.display = "none";
+	q3wrongText.style.color="red";
+	wrongSound.pause();
+	wrongSound.currentTime = 0;
+	wrongSound.play();
+	score--;
+	updateScoreboard();
 });
 
 q3wrong2.addEventListener("click", () => {
-  defaultImage3.src ="wrong.png";
-  defaultImage3.alt = "wrong image";
-  q3wrongText.style.display = "block";
-  q3correctText.style.display = "none";
-  q3wrongText.style.color="red";
-  wrongSound.pause();
-  wrongSound.currentTime = 0;
-  wrongSound.play();
-  score--;
-  updateScoreboard();
+	defaultImage3.src ="wrong.png";
+	defaultImage3.alt = "wrong image";
+	q3wrongText.style.display = "block";
+	q3correctText.style.display = "none";
+	q3wrongText.style.color="red";
+	wrongSound.pause();
+	wrongSound.currentTime = 0;
+	wrongSound.play();
+	score--;
+	updateScoreboard();
 });
